df74b47814ba3110c0b49032247c5e7c36839dfa
miaa9999 <toribo990228@gmail.com>
2024-06-07 18:11:49 +0900

    #01 프로젝트 기본 베이스 설정

---
+++ src/main/java/kr/co/homework/controller/mainController.java
@@ -0,0 +1,16 @@
+package kr.co.homework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+	
+	@GetMapping("/main")
+	public String TestMain() {
+		return "/mainPage";
+	}
+
+
+}
